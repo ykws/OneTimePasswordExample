70038e5311af604cbaa0d3a6f81bda430af2f2c7
岡慶太郎 <oka@okakeitarounoMacBook-Pro.local>
2021-09-30 23:05:51 +0900

    設定画面への遷移でエラー

---
+++ OneTimePasswordExample/SettingTableViewCell.h
@@ -0,0 +1,12 @@
+//
+//  SettingTableViewCell.h
+//  OneTimePasswordExample
+//
+//  Created by 岡慶太郎 on 2021/09/30.
+//
+
+#ifndef SettingTableViewCell_h
+#define SettingTableViewCell_h
+
+
+#endif /* SettingTableViewCell_h */
