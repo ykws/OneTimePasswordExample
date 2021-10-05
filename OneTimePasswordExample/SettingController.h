24434d6b61183397c2e2cb78a06d22c91fa8b546
Nakagami Mai <nm30.mmn0kg@gmail.com>
2021-10-05 23:23:38 +0900

    設定画面への遷移でエラー

---
+++ OneTimePasswordExample/SettingController.h
@@ -0,0 +1,16 @@
+//
+//  SettingController.h
+//  OneTimePasswordExample
+//
+//  Created by Mai Nakagami on 2021/09/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
