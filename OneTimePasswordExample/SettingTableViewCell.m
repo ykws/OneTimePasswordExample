d2e0a40e43203fe43d44745052de2e630308cce5
Nakagami Mai <nm30.mmn0kg@gmail.com>
2021-10-05 22:21:33 +0900

    設定画面を追加

--- OneTimePasswordExample/SettingTableViewCell.m
+++
@@ -1,8 +0,0 @@
-//
-//  SettingTableViewCell.m
-//  OneTimePasswordExample
-//
-//  Created by 岡慶太郎 on 2021/09/30.
-//
-
-#import <Foundation/Foundation.h>
---
+++ OneTimePasswordExample/SettingsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.m
+//  OneTimePasswordExample
+//
+//  Created by MaiNakagami on 2021/09/30.
+//
+
+#import "SettingTableViewCell.h"
+
+@implementation SettingsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
